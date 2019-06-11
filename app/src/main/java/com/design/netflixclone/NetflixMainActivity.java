ef1f97e4ef25c9543c4854ee52de2452f1b888bd
prasad091 <prasad.thangavel@gmail.com>
2019-06-11 15:35:09 +0530

    Netflix clone

---
+++ app/src/main/java/com/design/netflixclone/NetflixMainActivity.java
@@ -0,0 +1,22 @@
+package com.design.netflixclone;
+
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class NetflixMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+    }
+}
